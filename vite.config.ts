3200b1c1bfb999138a2859a0bca1d8ba429f7215
mayoro1972 <81039582+mayoro1972@users.noreply.github.com>
2026-04-08 18:44:17 +0100

    Fix GitHub Pages base path

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,10 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/",
+  base: loadEnv(mode, process.cwd(), "").VITE_BASE_PATH || "/",
   server: {
     host: "::",
     port: 8080,
