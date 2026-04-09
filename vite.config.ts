425fe036547b9c7a92666346f0168c113a2c8a92
mayoro1972 <81039582+mayoro1972@users.noreply.github.com>
2026-04-09 21:58:45 +0100

    Add legacy browser support for Safari

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react-swc";
+import legacy from "@vitejs/plugin-legacy";
 import path from "path";
 
 // https://vitejs.dev/config/
@@ -12,9 +13,14 @@ export default defineConfig(({ mode }) => ({
       overlay: false,
     },
   },
-  plugins: [react()],
+  plugins: [
+    react(),
+    legacy({
+      targets: ["defaults", "Safari >= 13"],
+      modernPolyfills: true,
+    }),
+  ],
   build: {
-    target: "es2019",
     rollupOptions: {
       output: {
         manualChunks(id) {
