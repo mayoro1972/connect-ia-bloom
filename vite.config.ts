8f13aa5b36c8bd90c15cb89b16bd8ddc7bff87d9
mayoro1972 <81039582+mayoro1972@users.noreply.github.com>
2026-04-09 22:38:52 +0100

    Remove Vite legacy bootstrap for Safari stability

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,5 @@
 import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react-swc";
-import legacy from "@vitejs/plugin-legacy";
 import path from "path";
 
 // https://vitejs.dev/config/
@@ -15,12 +14,9 @@ export default defineConfig(({ mode }) => ({
   },
   plugins: [
     react(),
-    legacy({
-      targets: ["defaults", "Safari >= 13"],
-      modernPolyfills: true,
-    }),
   ],
   build: {
+    target: "es2017",
     rollupOptions: {
       output: {
         manualChunks(id) {
