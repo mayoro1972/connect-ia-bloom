2519ca3fd08b8123d9f5962c74854f9c26790773
mayoro1972 <81039582+mayoro1972@users.noreply.github.com>
2026-04-17 19:18:26 +0100

    Updates

---
+++ assets/Webinars-C0hi9maw.js
@@ -0,0 +1,1 @@
+import{j as a}from"./index-CcLnkNjU.js";import{al as e}from"./App-DFhyYY84.js";const t=()=>a.jsx(e,{to:"/seminaires",replace:!0});export{t as default};
--- assets/Webinars-DNnsPlsI.js
+++
@@ -1,1 +0,0 @@
-import{j as a}from"./index-Bsxk57rq.js";import{al as e}from"./App-Cj0JlEBV.js";const t=()=>a.jsx(e,{to:"/seminaires",replace:!0});export{t as default};
